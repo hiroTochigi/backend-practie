b35c49ff8037f34c65db9ba0b1da6497a184fa8e
hiroTochigi <hiroyuki.terauchi.japan@gmail.com>
2020-08-01 18:40:50 -0500

    add test

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,7 +1,10 @@
 import express from 'express';
-import { indexPage } from "../controllers";
+import { indexPage, messagesPage } from '../controllers';
 const indexRouter = express.Router();
 
+console.log(indexPage)
+console.log(messagesPage)
 indexRouter.get('/', indexPage);
+indexRouter.get('/messages', messagesPage)
 
 export default indexRouter;

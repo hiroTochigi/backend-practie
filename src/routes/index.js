2235afa55d71b4125e65883eed0ed69c107c1d14
hiroTochigi <hiroyuki.terauchi.japan@gmail.com>
2020-08-02 12:20:26 -0500

    add middleware

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import { indexPage, messagesPage, nextPage, addMessage} from '../controllers';
+import { modifyMessage } from '../middleware';
 
 const indexRouter = express.Router();
 
@@ -8,5 +9,5 @@ console.log(messagesPage)
 indexRouter.get('/', indexPage);
 indexRouter.get('/messages', messagesPage)
 indexRouter.get('/test', nextPage);
-indexRouter.post('/messages', addMessage);
+indexRouter.post('/messages', modifyMessage, addMessage);
 export default indexRouter;
